d35664b155202c4212e794a1d6d731ef3c40542d
Oganesson897 <101081378+Darknight123MC@users.noreply.github.com>
2023-10-18 22:54:13 +0900

    Can It work?

---
+++ src/main/java/me/oganesson/gui/network/PktGUITransport.java
@@ -0,0 +1,42 @@
+package me.oganesson.gui.network;
+
+import java.util.UUID;
+import io.netty.buffer.ByteBuf;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import net.minecraftforge.fml.relauncher.Side;
+
+public class PktGUITransport implements IMessage, IMessageHandler<PktGUITransport, IMessage> {
+	private UUID uuid;
+
+	public PktGUITransport() {}
+
+	public PktGUITransport(UUID uuid) {
+		this.uuid = uuid;
+	}
+
+	@Override
+    public void toBytes(ByteBuf buf) {
+        buf.writeLong(this.uuid.getMostSignificantBits());
+        buf.writeLong(this.uuid.getLeastSignificantBits());
+    }
+
+    @Override
+    public void fromBytes(ByteBuf buf) {
+        final long most = buf.readLong();
+        final long least = buf.readLong();
+        this.uuid = new UUID(most, least);
+    }
+
+    @Override
+        public IMessage onMessage(PktGUITransport pkt , MessageContext ctx) {
+            if (ctx.side == Side.CLIENT) {
+                EntityPlayerSP player = Minecraft.getMinecraft().player;
+                if (player.getPersistentID() == pkt.uuid) return new PktGUITransport(pkt.uuid);
+            }
+            return null;
+        }
+}
